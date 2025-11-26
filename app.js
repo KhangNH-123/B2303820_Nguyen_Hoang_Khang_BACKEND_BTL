6417bca31da3b8f6ef3d6847a9ab23d096bf5966
KhangNH-123 <khangb2303820@student.ctu.edu.vn>
2025-11-26 20:48:21 +0700

    Dinh nghia cac route cho tai nguyen Book

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const contactsRouter = require("./app/routes/contact.route");
+const booksRouter = require("./app/routes/book.route");
 // const ApiError = require("./app/api-error");
 
 const app = express();
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to book manage application." });
 });
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/books", booksRouter);
 
 // app.use((req, res, next) => {
 //   return next(new ApiError(404, "Resource not found"));
